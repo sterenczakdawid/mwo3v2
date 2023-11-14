1df29ece3e05e4da7bcdf8f1543875fe1140c2af
Dawid <sterenczak.dawid@gmail.com>
2023-11-14 23:16:26 +0100

    version42

--- src/main/java/com/example/mwo3pipelines/controller/HomeController.java
+++ src/main/java/com/example/mwo3pipelines/controller/HomeController.java
@@ -1,19 +1,24 @@
 package com.example.mwo3pipelines.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @Value("${app.version}")
-    private String appVersion;
+    private static int version = 0;
 
     @GetMapping("/")
     public String home() {
-        return "Witaj w aplikacji na Azure 4\n" +
-                "\nNumer wersji: " + appVersion;
+        return "Witaj w aplikacji na Azure 555, wersja: " + getVersion() + "\n";
+    }
+
+    public static int getVersion() {
+        return version;
+    }
+
+    public static void incrementVersion() {
+        version++;
     }
 
 
